e75fc83851b77278e30987394acb86564d20cdf7
Jakub <sowinskik2009@gmail.com>
2026-06-18 19:52:51 +0200

    add animations

--- autostart.sh
+++ autostart.sh
@@ -6,4 +6,4 @@ set +e
 /usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 >/dev/null 2>&1 &
 wl-clip-persist --clipboard regular --reconnect-tries 0 >/dev/null 2>&1 &
 wl-paste --type text --watch cliphist store >/dev/null 2>&1 &
-noctalia >/dev/null 2>&1 &
+
