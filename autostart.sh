e00ecb8a7930e330d5feb239adbd1093345d7298
Jakub <sowinskik2009@gmail.com>
2026-06-26 17:55:27 +0200

    remove autostart script and move to mangowm exec-once

--- autostart.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-set +e
-
-/usr/lib/xdg-desktop-portal  >/dev/null 2>&1 &
-/usr/lib/polkit-gnome/polkit-gnome-authentication-agent-1 >/dev/null 2>&1 &
-wl-clip-persist --clipboard regular --reconnect-tries 0 >/dev/null 2>&1 &
-wl-paste --type text --watch cliphist store >/dev/null 2>&1 &
-
